49700e319458d83647be19b8b7ef950a133fa104
Noah Farr <login@noahfarr.com>
2025-05-08 23:24:40 +0200

    Continued work on DRQN

--- memory_rl/utils/__init__.py
+++ memory_rl/utils/__init__.py
@@ -1,3 +1,3 @@
 from .gae import compute_gae, compute_recurrent_gae
 from .wrapper import LogWrapper, RecordEpisodeStatistics
-from .exploration import epsilon_greedy
+from .exploration import epsilon_greedy, recurrent_epsilon_greedy

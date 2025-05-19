f1184c0b620e794f67a628beb4a5c4f13f97f2b6
Noah Farr <login@noahfarr.com>
2025-05-19 12:23:42 +0200

    Add BraxWrapper

--- memory_rl/utils/wrapper.py
+++ memory_rl/utils/wrapper.py
@@ -5,6 +5,8 @@ import chex
 import jax
 import jax.numpy as jnp
 import numpy as np
+from brax import envs
+from brax.envs.wrappers.training import AutoResetWrapper, EpisodeWrapper
 from flax import struct
 from gymnax.environments import environment, spaces
 
@@ -117,3 +119,35 @@ class LogWrapper(GymnaxWrapper):
         info["timestep"] = state.timestep
         info["returned_episode"] = done
         return obs, state, reward, done, info
+
+
+class BraxGymnaxWrapper:
+    def __init__(self, env_name, backend="positional"):
+        env = envs.get_environment(env_name=env_name, backend=backend)
+        env = EpisodeWrapper(env, episode_length=1000, action_repeat=1)
+        env = AutoResetWrapper(env)
+        self._env = env
+        self.action_size = env.action_size
+        self.observation_size = (env.observation_size,)
+
+    def reset(self, key, params=None):
+        state = self._env.reset(key)
+        return state.obs, state
+
+    def step(self, key, state, action, params=None):
+        next_state = self._env.step(state, action)
+        return next_state.obs, next_state, next_state.reward, next_state.done > 0.5, {}
+
+    def observation_space(self, params):
+        return spaces.Box(
+            low=-jnp.inf,
+            high=jnp.inf,
+            shape=(self._env.observation_size,),
+        )
+
+    def action_space(self, params):
+        return spaces.Box(
+            low=-1.0,
+            high=1.0,
+            shape=(self._env.action_size,),
+        )

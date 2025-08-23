27c43dda47253f849568bc6a4d43f52d5417d71d
Noah Farr <login@noahfarr.com>
2025-08-23 23:38:02 +0200

    Add Craftax

--- memory_rl/utils/__init__.py
+++ memory_rl/utils/__init__.py
@@ -1,5 +1,10 @@
 from .exploration import epsilon_greedy, recurrent_epsilon_greedy
 from .gae import compute_gae, compute_recurrent_gae
 from .update import delayed_update, periodic_incremental_update
-from .wrapper import BraxGymnaxWrapper, LogWrapper, NavixGymnaxWrapper, VecEnv
+from .wrapper import (
+    BraxGymnaxWrapper,
+    LogWrapper,
+    NavixGymnaxWrapper,
+    PixelCraftaxEnvWrapper,
+)
 from .decorators import callback

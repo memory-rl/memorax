d3b2b13eae37e8bc8a971a444437d34c88a202cf
Noah Farr <login@noahfarr.com>
2025-08-30 13:55:22 +0200

    Add xLSTM

--- memory_rl/networks/recurrent/xlstm/__init__.py
+++ memory_rl/networks/recurrent/xlstm/__init__.py
@@ -1,2 +1,5 @@
 from .mlstm import mLSTMCell, mLSTMBlock
 from .slstm import sLSTMCell, sLSTMBlock
+from .xlstm import xLSTMCell
+
+__all__ = ["sLSTMCell", "sLSTMBlock", "mLSTMCell", "mLSTMBlock", "xLSTMCell"]

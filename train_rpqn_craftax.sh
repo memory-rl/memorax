94a92ec4c0981bb9524e9234dc329434a35f6fcf
farr <noah.farr@stud.tu-darmstadt.de>
2025-09-02 22:36:03 +0200

    Update rpqn conf

--- train_rpqn_craftax.sh
+++ train_rpqn_craftax.sh
@@ -1,23 +1,5 @@
 python main.py -m \
-  algorithm=rppo \
-  algorithm/torso=optimized_lstm \
-  environment=craftax_symbolic \
-seed=0,1,2,3,4 \
-logger=[wandb,file] \
-hydra/launcher=lichtenberg &
-
-python main.py -m \
-  algorithm=rppo \
-  algorithm/torso=gru \
-  algorithm/torso=gru \
-  environment=craftax_symbolic \
-seed=0,1,2,3,4 \
-logger=[wandb,file] \
-hydra/launcher=lichtenberg &
-
-
-python main.py -m \
-  algorithm=rppo \
+  algorithm=rpqn \
   algorithm/torso=slstm \
   algorithm/torso=slstm \
   environment=craftax_symbolic \
@@ -26,7 +8,7 @@ logger=[wandb,file] \
 hydra/launcher=lichtenberg &
 
 python main.py -m \
-  algorithm=rppo \
+  algorithm=rpqn \
   algorithm/torso=mlstm \
   algorithm/torso=mlstm \
   environment=craftax_symbolic \
@@ -35,7 +17,7 @@ logger=[wandb,file] \
 hydra/launcher=lichtenberg &
 
 python main.py -m \
-  algorithm=rppo \
+  algorithm=rpqn \
   algorithm/torso=xlstm \
   algorithm/torso=xlstm \
   environment=craftax_symbolic \
@@ -44,18 +26,9 @@ logger=[wandb,file] \
 hydra/launcher=lichtenberg &
 
 python main.py -m \
-  algorithm=rppo \
+  algorithm=rpqn \
   algorithm/torso=gtrxl \
   algorithm/torso=gtrxl \
-  environment=craftax_symbolic \
-seed=0,1,2,3,4 \
-logger=[wandb,file] \
-hydra/launcher=lichtenberg &
-
-python main.py -m \
-  algorithm=rppo \
-  algorithm/torso=shm \
-  algorithm/torso=shm \
   environment=craftax_symbolic \
 seed=0,1,2,3,4 \
 logger=[wandb,file] \

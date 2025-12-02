4bdf8833d42fba043d1072b1f79d96611b86ae84
noahfarr <login@noahfarr.com>
2025-12-02 12:55:23 +0100

    .

--- memory_rl/networks/recurrent/xlstm.py
+++ memory_rl/networks/recurrent/xlstm.py
@@ -5,17 +5,37 @@ from typing import (
 )
 
 from jax import random
+import jax.numpy as jnp
 import flax.linen as nn
 from flax.linen.module import compact, nowrap
 from flax.linen.recurrent import RNNCellBase
 
 from .slstm import sLSTMLayer
 from .mlstm import mLSTMLayer
+from .utils import small_init, wang_init
 
 A = TypeVar("A")
 Carry = Any
 CarryHistory = Any
 Output = Any
+
+class GatedFeedForward(nn.Module):
+    up_proj_dim: int
+    down_proj_dim: int
+    dropout_rate: float = 0.0
+
+    @compact
+    def __call__(self, x):
+        *_, in_features = x.shape
+        x = nn.Dense(2 * self.up_proj_dim, name="up_proj", kernel_init=small_init(in_features), use_bias=False)(x)
+        gate, x = jnp.split(x, 2, axis=-1)
+        x = nn.Dense(self.down_proj_dim, name="down_proj", kernel_init=wang_init(dim=in_features, num_blocks=1), use_bias=False)(
+            nn.gelu(gate) * x
+        )
+        x = nn.Dropout(rate=self.dropout_rate)(x)
+        return x
+
+
 
 
 class xLSTMBlock(nn.Module):
@@ -33,7 +53,7 @@ class xLSTMBlock(nn.Module):
 
         if self.ffn is not None:
             skip = x
-            x = nn.LayerNorm(use_bias=False, name="post_ln")(x)
+            x = nn.LayerNorm(use_bias=False, name="ffn_ln")(x)
             x = self.ffn(x) + skip
         return carry, x
 
@@ -61,6 +81,10 @@ class xLSTMCell(RNNCellBase):
                         features=self.features,
                         hidden_dim=self.hidden_dim,
                         name=f"sLSTMCell_{i}",
+                    ),
+                    ffn=GatedFeedForward(
+                        up_proj_dim=4/3 * self.hidden_dim,
+                        down_proj_dim=self.hidden_dim,
                     ),
                     kernel_init=self.kernel_init,
                     bias_init=self.bias_init,

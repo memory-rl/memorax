07adb3feaa2d1c8b74b1365d4b47e89fe418b790
Noah Farr <login@noahfarr.com>
2025-12-21 13:59:14 +0100

    Fix CartPole

--- memorax/environments/popjym.py
+++ memorax/environments/popjym.py
@@ -39,86 +39,6 @@ max_steps_in_episode = {
 }
 
 
-class AliasPrevActionV2(GymnaxWrapper):
-
-    def observation_space(self, params):
-        action_space = self._env.action_space(params)
-        if type(action_space) == spaces.Discrete:
-            low = jnp.concatenate(
-                [
-                    self._env.observation_space(params).low,
-                    jnp.zeros((action_space.n + 1,)),
-                ]
-            )
-            high = jnp.concatenate(
-                [
-                    self._env.observation_space(params).high,
-                    jnp.ones((action_space.n + 1,)),
-                ]
-            )
-            return spaces.Box(
-                low=low,
-                high=high,
-                shape=(
-                    self._env.observation_space(params).shape[-1] + action_space.n + 1,
-                ),
-                dtype=self._env.observation_space(params).dtype,
-            )
-        elif type(action_space) == spaces.Box:
-            low = jnp.concatenate(
-                [
-                    self._env.observation_space(params).low,
-                    jnp.array([action_space.low]),
-                    jnp.array([0.0]),
-                ]
-            )
-            high = jnp.concatenate(
-                [
-                    self._env.observation_space(params).high,
-                    jnp.array([action_space.high]),
-                    jnp.array([1.0]),
-                ]
-            )
-            return spaces.Box(
-                low=low,
-                high=high,
-                shape=(self._env.observation_space(params).shape[-1] + 2,),
-                dtype=self._env.observation_space(params).dtype,
-            )
-        else:
-            raise NotImplementedError
-
-    @partial(jax.jit, static_argnums=(0,))
-    def reset(self, key, params=None):
-        action_space = self._env.action_space(params)
-        obs, state = self._env.reset(key, params)
-        if isinstance(action_space, spaces.Box):
-            obs = jnp.concatenate([obs, jnp.array([0.0, 1.0])])
-        elif isinstance(action_space, spaces.Discrete):
-            obs = jnp.concatenate([obs, jnp.zeros((action_space.n,)), jnp.array([1.0])])
-        else:
-            raise NotImplementedError
-        return obs, state
-
-    @partial(jax.jit, static_argnums=(0,))
-    def step(
-        self,
-        key,
-        state,
-        action,
-        params=None,
-    ):
-        obs, state, reward, done, info = self._env.step(key, state, action, params)
-        action_space = self._env.action_space(params)
-        if isinstance(action_space, spaces.Discrete):
-            action_in = jnp.zeros((action_space.n,))
-            action_in = action_in.at[action].set(1.0)
-            obs = jnp.concatenate([obs, action_in, jnp.array([0.0])])
-        else:
-            obs = jnp.concatenate([obs, action, jnp.array([0.0])])
-        return obs, state, reward, done, info
-
-
 @dataclass(frozen=True)
 class EnvParams:
     env_params: Any
@@ -128,7 +48,8 @@ class EnvParams:
 class PopJaxRLWrapper(GymnaxWrapper):
     def __init__(self, env):
         super().__init__(env)
-        self.max_steps_in_episode = max_steps_in_episode[env.name]
+        env_id = type(env).__name__
+        self.max_steps_in_episode = max_steps_in_episode[env_id]
 
     def reset(self, key, params):
         return self._env.reset(key, params.env_params)
@@ -144,7 +65,6 @@ def make(env_id, **kwargs):
     import popjym
 
     env, env_params = popjym.make(env_id, **kwargs)
-    env = AliasPrevActionV2(env)
     env = PopJaxRLWrapper(env)
     env_params = EnvParams(
         env_params=env_params, max_steps_in_episode=max_steps_in_episode[env_id]

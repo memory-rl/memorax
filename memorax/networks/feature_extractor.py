1b9b86e5cefcf6b6449aa978a2cec7b7360cb5b4
noahfarr <login@noahfarr.com>
2025-12-30 22:42:13 +0100

    Make concatenation optional

--- memorax/networks/feature_extractor.py
+++ memorax/networks/feature_extractor.py
@@ -9,15 +9,16 @@ class FeatureExtractor(nn.Module):
     observation_extractor: nn.Module
     action_extractor: Optional[nn.Module] = None
     reward_extractor: Optional[nn.Module] = None
+    concatenate: bool = True
 
     def extract(
         self,
-        features: list,
+        features: tuple,
         extractor: Optional[nn.Module],
         x: Optional[jnp.ndarray] = None,
     ):
         if extractor is not None and x is not None:
-            features.append(extractor(x))
+            features += (extractor(x),)
 
     @nn.compact
     def __call__(
@@ -26,9 +27,12 @@ class FeatureExtractor(nn.Module):
         action: Optional[jnp.ndarray] = None,
         reward: Optional[jnp.ndarray] = None,
         **kwargs,
-    ) -> jnp.ndarray:
-        features = [self.observation_extractor(observation)]
+    ):
+        features = (self.observation_extractor(observation),)
         self.extract(features, self.action_extractor, action)
         self.extract(features, self.reward_extractor, reward)
 
-        return jnp.concatenate(features, axis=-1)
+        if self.concatenate:
+            features = jnp.concatenate(features, axis=-1)
+
+        return features

1f3f6dc4e5275ae3e66b62af5e526f5deab11d09
noahfarr <login@noahfarr.com>
2025-12-31 16:25:47 +0100

    Remove PPO continuous

--- memorax/algorithms/__init__.py
+++ memorax/algorithms/__init__.py
@@ -1,6 +1,5 @@
 from .dqn import DQN, DQNConfig, DQNState
 from .ppo import PPO, PPOConfig, PPOState
-from .ppo_continuous import PPOContinuous, PPOContinuousConfig, PPOContinuousState
 from .sac import SAC, SACConfig, SACState
 from .sacd import SACD, SACDConfig, SACDState
 from .pqn import PQN, PQNConfig, PQNState

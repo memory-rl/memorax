d4afbf4d694900aaaeb66106c8a52c130d4b31bd
noahfarr <login@noahfarr.com>
2026-01-09 18:26:34 +0100

    Make wrapper pass kwargs

--- memorax/networks/sequence_models/wrapper.py
+++ memorax/networks/sequence_models/wrapper.py
@@ -8,7 +8,7 @@ class RecurrentWrapper(nn.Module):
 
     def __call__(self, inputs, mask, initial_carry=None, **kwargs):
         carry = initial_carry
-        return carry, self.network(inputs)
+        return carry, self.network(inputs, **kwargs)
 
     def initialize_carry(self, rng, input_shape):
         return jnp.zeros(input_shape)

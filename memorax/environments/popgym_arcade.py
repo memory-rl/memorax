fcc134e42ec34233748fb9d80c0d1631381418ba
noahfarr <login@noahfarr.com>
2026-01-11 21:10:48 +0100

    Fix wrapper

--- memorax/environments/popgym_arcade.py
+++ memorax/environments/popgym_arcade.py
@@ -2,16 +2,18 @@ from typing import Any
 
 from flax import struct
 
+
 @struct.dataclass(frozen=True)
 class EnvParams:
     env_params: Any
-    max_steps_in_episode
+    max_steps_in_episode: int
+
 
 def make(env_id, **kwargs):
     import popgym_arcade
 
     env, env_params = popgym_arcade.make(env_id, **kwargs)
     env_params = EnvParams(
-        env_params=env_params, max_steps_in_episode=env._env.max_steps_in_episode
+        env_params=env_params, max_steps_in_episode=env.max_steps_in_episode
     )
     return env, env_params

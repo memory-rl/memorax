ea26a58e148ef31c6367f3d299f4d2d31de861ea
noahfarr <login@noahfarr.com>
2026-01-12 20:03:38 +0100

    Remove unused sequence models

--- memorax/networks/sequence_models/sequence_model.py
+++ memorax/networks/sequence_models/sequence_model.py
@@ -1,21 +1,22 @@
 from abc import ABC, abstractmethod
+from typing import Optional
 
 import flax.linen as nn
 
 from memorax.utils.typing import Array, Carry
 
+
 class SequenceModel(ABC, nn.Module):
+    features: int
 
     @abstractmethod
     def __call__(
         self,
         inputs: Array,
         mask: Array,
-        initial_carry: Carry,
+        initial_carry: Optional[Carry] = None,
         **kwargs,
-    ):
-        pass
+    ) -> tuple: ...
 
     @abstractmethod
-    def initialize_carry(self, key, input_shape):
-        pass
+    def initialize_carry(self, key, input_shape) -> Carry: ...
